b02b6ba2519d49a40e8efdfe74f161b27fe21e83
JOONHYUN, JEONG <terriyou@naver.com>
2017-12-26 13:27:33 +0900

    add contents
    gugudan

--- cpp.cpp
+++ cpp.cpp
@@ -1,1 +1,13 @@
 #include <iostream>
+using namespace std;
+int main(void)
+{ 
+  for (int b = 1; b < 10; b++) 
+  {
+    cout << b << "단\n";  
+    for (int a = 1; a < 10; a++)  
+    {   
+      cout << b << " * " << a << " = " << b * a << endl;  
+    }  
+    cout << "\n\n"; }
+}
